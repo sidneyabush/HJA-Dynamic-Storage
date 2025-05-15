d766bef83697024aabf47a223f75b6ee4cd01a3c
Sidney Bush <bushsi@oregonstate.edu>
2025-05-15 15:25:56 -0700

    Update Create_Master_MET_Dataset.R

---
+++ Create_Master_MET_Dataset.R
@@ -0,0 +1,16 @@
+library(readr)
+library(dplyr)
+library(lubridate)
+
+# ── Clear environment ─────────────────────────────────────────────────────────
+rm(list = ls())
+
+# ── Directories & site list ─────────────────────────────────────────────────
+base_dir   <- "/Users/sidneybush/Library/CloudStorage/Box-Box/05_Storage_Manuscript/03_Data"
+
+# Need: 
+# Precip
+# Net Radiation
+# Air Temp
+# RH
+
