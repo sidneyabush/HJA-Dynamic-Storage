41391f4f11b092de25f2157e8cc0c8973249747d
Sidney Bush <bushsi@oregonstate.edu>
2025-05-15 20:43:56 -0700

    Update Create_Master_MET_Dataset.R

--- Create_Master_MET_Dataset.R
+++ Create_Master_MET_Dataset.R
@@ -8,9 +8,15 @@ rm(list = ls())
 # ── Directories & site list ─────────────────────────────────────────────────
 base_dir   <- "/Users/sidneybush/Library/CloudStorage/Box-Box/05_Storage_Manuscript/03_Data"
 
-# Need: 
+# Need to import: 
 # Precip
 # Net Radiation
 # Air Temp
 # RH
 
+# The databases and dates will vary by watershed
+# GSLOOK (the entirety of the watershed) will need to be averaged or interpolated
+
+# Need to decide a method for calculating ET
+# Latitude method?
+# Calculate Ground Heat Flux
